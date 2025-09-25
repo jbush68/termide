12e20c7125470176e96ec63fe76440d709ba6b35
hrtty <hrtty@nixos.pluh>
2025-09-25 16:11:36 -0600

    0.1.4a

--- homes/configs/zsh/hrtty-zsh.nix
+++ homes/configs/zsh/hrtty-zsh.nix
@@ -5,8 +5,8 @@
   # cdpath
   # completionInit
   # defaultKeymap
-  # dirHashes = {};
-  # dotDir
+  dirHashes = { systemconfigs = "./etc/nixos"; };
+  dotDir = "`\${config.home.homeDirectory}/hm`";
   enable = true;
   enableCompletion = true;
   # enableVteIntegration
@@ -24,6 +24,79 @@
   # shellAliases
   # shellGlobalAliases
   # siteFunctions
+
+
+  ### Antidote ###
+  # Zsh Plugin manager
   
+  
+  ### AutoSuggestion ###
+  autosuggestion = {
+    enable = true;
+    highlight = "fg=cyan,underline";
+    strategy = [ "completion" "history" ];
+  };
+
+
+  ### History ###
+  history = {
+    append = true;
+    expireDuplicatesFirst = true;
+    extended = true;
+    # findNoDups
+    # ignoreAllDups
+    ignoreDups = true;
+    # ignorePatterns
+    # ignoreSpace
+    path = "`\${config.programs.zsh.dotDir}/.zsh_history`";
+    save = 7500;
+    saveNoDups = true;
+    share = true;
+    size = 7500;
+  };
+  
+
+  ### History Substring Search ###
+  historySubstringSearch = {
+    enable = true;
+    #searchDownKey
+    #searchUpKey
+  };
+
+
+  ### Oh My Zsh ###
+  # Replaced by Oh-My-Posh ($user-omp.nix)
+
+
+  ### Plugins ###
+  # Do Not Use (plugin managers are better for this)
+
+
+  ### Prezto ###
+  # Anti-pattern --> Prezto is a configuration framework... which this already is
+
+
+  ### Syntax Highlighting ###
+  syntaxHighlighting = {
+    enable = true;
+    highlighters = [ "main" "brackets" "root" ];
+    # package
+    # patterns
+    # styles
+  };
+
+  
+  ### Zplug ###
+  # *Another* plugin manager
+
+
+  ### Zprof ###
+  zprof = {
+    enable = false; # Only enable for debugging long inits
+  };
+
+
+  ### Zsh-Abbr ###
+  # Manages aliases and abbreviations
 
 }
