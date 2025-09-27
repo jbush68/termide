c76a80be28e677964557a73a234cc28dc536e79f
hrtty <hrtty@nixos.pluh>
2025-09-27 02:28:35 -0600

    v0.1.6h

--- homes/configs/terminfo/hrtty-btop.nix
+++ homes/configs/terminfo/hrtty-btop.nix
@@ -6,19 +6,19 @@
   # package
   extraConfig = ''
     color_theme = "TTY"
-    # theme_background = false;
-    # btruecolor = false;
-    # force_tty = false;
-    # vim_keys = false;
-    # rounded_corners = false;
-    # graph_symbol = "braille";
-    # update_ms = 1000;
-    # proc_tree = true;
-    # proc_gradient = false;
-    # proc_left = true;
-    # clock_format = "/user@/host | %F|%T | /uptime";
-    # background_update = false;
-    # mem_graphs = false;
+    theme_background = False
+    btruecolor = false;
+    force_tty = false;
+    vim_keys = false;
+    rounded_corners = False
+    graph_symbol = "braille";
+    update_ms = 1000
+    proc_tree = true;
+    proc_gradient = false;
+    proc_left = true;
+    clock_format = "/user@/host | %F|%T | /uptime";
+    background_update = false;
+    mem_graphs = false;
     show_battery = False
   '';
   # themes
