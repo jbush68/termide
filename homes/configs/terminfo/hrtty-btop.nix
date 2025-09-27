43038e62c814354bded0fa717b3983601b5ce4e0
hrtty <hrtty@nixos.pluh>
2025-09-27 02:04:39 -0600

    v0.1.6c

--- homes/configs/terminfo/hrtty-btop.nix
+++ homes/configs/terminfo/hrtty-btop.nix
@@ -11,14 +11,14 @@
     force_tty = false;
     # vim_keys = false;
     rounded_corners = false;
-    # graph_symbol = "braille";
-    # update_ms = 1000;
-    # proc_tree = true;
-    # proc_gradient = false;
+    graph_symbol = "braille";
+    update_ms = 1000;
+    proc_tree = true;
+    proc_gradient = false;
     # proc_left = true;
     # clock_format = "/user@/host | %F|%T | /uptime";
     background_update = false;
-    # mem_graphs = false;
+     mem_graphs = false;
     show_battery = false;
   };
   # themes
