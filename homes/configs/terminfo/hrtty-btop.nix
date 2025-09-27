6b4af849acb63a756ee40a0769da8fcc7b5bda53
hrtty <hrtty@nixos.pluh>
2025-09-27 03:02:29 -0600

    v0.1.7

--- homes/configs/terminfo/hrtty-btop.nix
+++ homes/configs/terminfo/hrtty-btop.nix
@@ -1,25 +1,25 @@
 {
   ### Top Level Config ###
 
-  enable = true;
+  enable = true; # --> Todo: Why can you not configure this shit beforehand?!
   # extraConfig
   # package
-  extraConfig = ''
-    color_theme = "TTY"
-    theme_background = False
-    btruecolor = False
-    force_tty = False
-    vim_keys = False
-    rounded_corners = False
-    graph_symbol = "braille"
-    update_ms = 1000
-    proc_tree = True
-    proc_gradient = False
-    proc_left = True
-    clock_format = "/user@/host | %F|%T | /uptime"
-    background_update = False
-    mem_graphs = False
-    show_battery = False
-  '';
+  # extraConfig = ''
+  #   color_theme = "TTY"
+  #   theme_background = False
+  #   btruecolor = False
+  #   force_tty = False
+  #   vim_keys = False
+  #   rounded_corners = False
+  #   graph_symbol = "braille"
+  #   update_ms = 1000
+  #   proc_tree = True
+  #   proc_gradient = False
+  #   proc_left = True
+  #   clock_format = "/user@/host | %F|%T | /uptime"
+  #   background_update = False
+  #   mem_graphs = False
+  #   show_battery = False
+  # '';
   # themes
 }
